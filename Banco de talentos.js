fdc75b7748e7e7e51fa3b2278f7f05ff08d0a173
gabriel-klein <49499111+gabriel-klein@users.noreply.github.com>
2019-04-10 20:37:31 -0300

    Update Banco de talentos.js

--- Banco de talentos.js
+++ Banco de talentos.js
@@ -4,3 +4,10 @@ function trabalhei()
              
     div.innerHTML = "<input type='checkbox' name='operador' id='operador'/>Operador(a) de Caixa<p><input type='checkbox' name='auxiliar' id='aulixiar'/>Auxiliar de Serviços Gerais<p><input type='checkbox' name='vendedor' id='vendedor'/>Vendedor(a) <p><input type='checkbox' name='coordenador' id='coordenador'/>Coordenador(a)/Gerente de Loja<p><input type='checkbox' name='vigia' id='vigia'/>Vigia/Prevenção de perdas<p><input type='checkbox' name='estoquista' id='estoquista'/>Estoquista<p><input type='checkbox' name='baba' id='baba'/>Babá/Cuidador<p><input type='checkbox' name='estimulador' id='estimulador'/>Estimulador<p><input type='checkbox' name='cozinheiro' id='cozinheiro'/>Cozinheiro<p><input type='checkbox' name='garçom' id='garçom'/>Garçom/Garçonete<p><input type='checkbox' name='atendente' id='atendente'/>Atendente de Telemarketing<p><input type='checkbox' name='frentista' id='frentista'/>Frentista<p>Outra função? Qual? <input type'text' name='outra' id='outra' />";
 }
+function apaga()
+{
+	var div = document.getElementById("divResultado");
+             
+    div.innerHTML = "";
+}
+document.getElementById("teste").style.backgroundColor = "red";
