b486ec73f0a9401892d3b9561135a21c7d250cde
gabriel-klein <49499111+gabriel-klein@users.noreply.github.com>
2019-04-10 18:30:43 -0300

    Add files via upload

---
+++ Banco de talentos.js
@@ -0,0 +1,6 @@
+function trabalhei()
+{
+	var div = document.getElementById("divResultado");
+             
+    div.innerText = "<p>"+Operador de caixa + "<br>" + <input type='checkbox' name='operador' id='operador'/>";
+}
