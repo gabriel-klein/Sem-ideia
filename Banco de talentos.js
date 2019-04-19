c36d93b3190096656210fa68cdf6168e7b23e8fe
gabriel-klein <49499111+gabriel-klein@users.noreply.github.com>
2019-04-19 17:52:19 -0300

    Update Banco de talentos.js

--- Banco de talentos.js
+++ Banco de talentos.js
@@ -2,7 +2,7 @@ function trabalhei()
 {
 	var div = document.getElementById("divResultado");
              
-    div.innerHTML = "<input type='checkbox' name='operador' id='operador'/>Operador(a) de Caixa<p><input type='checkbox' name='auxiliar' id='aulixiar'/>Auxiliar de Serviços Gerais<p><input type='checkbox' name='vendedor' id='vendedor'/>Vendedor(a) <p><input type='checkbox' name='coordenador' id='coordenador'/>Coordenador(a)/Gerente de Loja<p><input type='checkbox' name='vigia' id='vigia'/>Vigia/Prevenção de perdas<p><input type='checkbox' name='estoquista' id='estoquista'/>Estoquista<p><input type='checkbox' name='baba' id='baba'/>Babá/Cuidador<p><input type='checkbox' name='estimulador' id='estimulador'/>Estimulador<p><input type='checkbox' name='cozinheiro' id='cozinheiro'/>Cozinheiro<p><input type='checkbox' name='garçom' id='garçom'/>Garçom/Garçonete<p><input type='checkbox' name='atendente' id='atendente'/>Atendente de Telemarketing<p><input type='checkbox' name='frentista' id='frentista'/>Frentista<p>Outra função? Qual? <input type'text' name='outra' id='outra' />";
+    div.innerHTML = "<input type='checkbox' name='operador' id='operador'/>Operador(a) de Caixa<p><input type='checkbox' name='auxiliar' id='aulixiar'/>Auxiliar de Serviços Gerais<p><input type='checkbox' name='vendedor' id='vendedor'/>Vendedor(a) <p><input type='checkbox' name='coordenador' id='coordenador'/>Coordenador(a)/Gerente de Loja<p><input type='checkbox' name='vigia' id='vigia'/>Vigia/Prevenção de perdas<p><input type='checkbox' name='estoquista' id='estoquista'/>Estoquista<p><input type='checkbox' name='baba' id='baba'/>Babá/Cuidador<p><input type='checkbox' name='estimulador' id='estimulador'/>Estimulador<p><input type='checkbox' name='cozinheiro' id='cozinheiro'/>Cozinheiro<p><input type='checkbox' name='garçom' id='garçom'/>Garçom/Garçonete<p><input type='checkbox' name='atendente' id='atendente'/>Atendente de Telemarketing<p><input type='checkbox' name='frentista' id='frentista'/>Frentista<p>Outra função? Qual? <input type'text' name='outra' id='outra' onkeypress='return somente_letra()' />";
 }
 function apaga()
 {
@@ -11,6 +11,7 @@ function apaga()
     div.innerHTML = "";
 }
 //document.getElementById("teste").style.backgroundColor = "red";
+
 
 function somente_num(campo)
 {
@@ -37,4 +38,14 @@ function somente_num(campo)
 	else
 	return false;
 
+}
+
+function somente_letra()
+{
+	if((event.charCode>=65 && event.charCode <=90)||(event.charCode>=97 && event.charCode<=122))
+	{
+		return true;
+	}
+	else
+		return false;
 }
