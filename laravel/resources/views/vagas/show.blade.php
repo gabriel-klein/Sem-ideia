636eaa501de7c0018b2ac1878b9593771cf8c708
João <thej@tutamail.com>
2020-03-07 18:32:45 -0300

    Mostrando usuários nas vagas da empresa

--- laravel/resources/views/vagas/show.blade.php
+++ laravel/resources/views/vagas/show.blade.php
@@ -3,16 +3,29 @@
 @section('titulo', 'Descrição da Vaga')
 
 @section('conteudo')
-  <h2 class=" text-center">{{ $vaga->funcao }}</h3>
-  <h4 class="h4">Descrição</h5>
+  <h2 class=" text-center">{{ $vaga->funcao }}</h2>
+    
+  <h4 class="h4">Descrição</h4>
   <p>{{ $vaga->descricao }}</p>
   <p>Quantidade de Vagas: {{ $vaga->quantidade }}</p>
-  <h4 class="h4">Requisitos</h5>
-  @foreach ($vaga->conhecimentos as $conhecimento)
+  
+  <h4 class="h4">Requisitos</h4>
+    @foreach ($vaga->conhecimentos as $conhecimento)
       <p class="my-1">{{ $conhecimento->nome." - ".$conhecimento->pivot->nivel }}</p>
-  @endforeach
+    @endforeach
+  
   <h5 class="h5 mt-2">E-mail de contato</h5>
   <p>{{ $vaga->email_de_contato }}</p>
+
+  @emp 
+    <h5>Candidatos</h5>
+    @forelse ($vaga->clientes as $cliente)
+        <p><a href="{{ route('cliente.show', $cliente->id) }}"  data-remote="true">{{$cliente->user->name}}</a></p>
+    @empty
+        <p>Não há pessoas interessadas a vaga</p>
+    @endforelse
+  @endemp
+
 @endsection
 
 @section('footer')
